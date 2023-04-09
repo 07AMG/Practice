3ea8e362cc2ee4e1cba51d91e098fef8e5434d81
Ankit G <ankitgodwe@gmail.com>
2023-04-09 14:54:10 +0530

    B2 class added

---
+++ src/gitPractice/B2.java
@@ -0,0 +1,14 @@
+package gitPractice;
+
+public class B2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1;
+String a="ankit";
+char b='a';
+System.out.println(a+b+i);
+
+	}
+
+}
